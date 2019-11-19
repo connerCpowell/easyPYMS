0160a062d60f2e8518c1c58f502ec21560645cc3
Conner C Powell <ccpowell@ucsc.edu>
2019-11-18 18:04:32 -0800

    Add files via upload
    user-input pipeline proto

---
+++ berryPipe.py
@@ -0,0 +1,218 @@
+import sys
+from pyms.GCMS.IO.ANDI.Function import ANDI_reader
+from pyms.GCMS.Function import build_intensity_matrix
+from pyms.Noise.SavitzkyGolay import savitzky_golay
+from pyms.Baseline.TopHat import tophat
+from pyms.Deconvolution.BillerBiemann.Function import BillerBiemann, rel_threshold, num_ions_threshold
+from pyms.Peak.Class import Peak
+from pyms.Peak.Function import peak_sum_area
+from pyms.Experiment.Class import Experiment
+from pyms.Experiment.IO import store_expr
+import itertools
+import fnmatch
+import os
+from pyms.Noise.Analysis import window_analyzer
+import csv
+
+
+
+def glob(glob_pattern, directoryname):
+    '''
+    Walks through a directory and its subdirectories looking for files matching
+    the glob_pattern and returns a list=[].
+
+    :param directoryname: Any accessible folder name on the filesystem.
+    :param glob_pattern: A string like "*.txt", which would find all text files.
+    :return: A list=[] of absolute filepaths matching the glob pattern.
+    '''
+    matches = []
+    names = []
+    for root, dirnames, filenames in os.walk(directoryname):
+        for filename in fnmatch.filter(filenames, glob_pattern):
+            absolute_filepath = os.path.join(root, filename)
+            matches.append(absolute_filepath)
+
+            name = filename.rsplit('/CDFdata/')[-1]
+            names.append(name)
+
+    return matches, names
+
+def matrix_from_cdf(cdffile, name):
+    data = ANDI_reader(cdffile)
+    #print(name)
+    #data.info()
+    tic = data.get_tic()
+    noise_lvl = window_analyzer(tic)
+    #print(noise_lvl)
+
+    return build_intensity_matrix(data), noise_lvl
+
+
+def Preprocess_IntensityMatrixes(matrixes):
+    # noise removal and baseline correction of Intensity Matricies
+    #input matrix list, outputs corrected matrix list
+
+    count = 1
+    for im in matrixes:
+
+        n_s, n_mz = im.get_size()
+        count += 1
+
+        for ii in range(n_mz):
+
+            #print("Working on IC#", ii+1, " Unit", count)
+            ic = im.get_ic_at_index(ii)
+            ic_smoof = savitzky_golay(ic)
+            ic_bc = tophat(ic_smoof, struct='1.5m')
+            im.set_ic_at_index(ii, ic_bc)
+
+    #print(matrixes)
+    return(matrixes)        #save to file
+
+
+
+def Peak_detector(pp_im, noise, name, points, scans, percent, nin, name_tag, sdir):
+    # Peak detection and filtering and selection
+    peakz = []
+    counter = 1
+    savePath = sdir
+    ms_data_files = []
+
+    for im, n, na in itertools.izip(list(pp_im), noise, name):
+
+        ms_data = []
+
+        #print(na)
+        poss_peaks = BillerBiemann(im, points=points, scans=scans)                 #increase scan #
+        pi = rel_threshold(poss_peaks, percent=percent)
+        nin = num_ions_threshold(pi, n=nin, cutoff=n)
+
+
+
+        completeName = os.path.join(savePath, na + name_tag + "ms_data.csv")
+        with open(completeName, 'w') as f:
+            w = csv.writer(f)
+            head = ['RTs', 35.0, 36.0, 37.0, 38.0, 39.0, 40.0, 41.0, 42.0, 43.0, 44.0, 45.0, 46.0, 47.0, 48.0, 49.0, 50.0, 51.0, 52.0, 53.0, 54.0, 55.0, 56.0, 57.0, 58.0, 59.0, 60.0, 61.0, 62.0, 63.0, 64.0, 65.0, 66.0, 67.0, 68.0, 69.0, 70.0, 71.0, 72.0, 73.0, 74.0, 75.0, 76.0, 77.0, 78.0, 79.0, 80.0, 81.0, 82.0, 83.0, 84.0, 85.0, 86.0, 87.0, 88.0, 89.0, 90.0, 91.0, 92.0, 93.0, 94.0, 95.0, 96.0, 97.0, 98.0, 99.0, 100.0, 101.0, 102.0, 103.0, 104.0, 105.0, 106.0, 107.0, 108.0, 109.0, 110.0, 111.0, 112.0, 113.0, 114.0, 115.0, 116.0, 117.0, 118.0, 119.0, 120.0, 121.0, 122.0, 123.0, 124.0, 125.0, 126.0, 127.0, 128.0, 129.0, 130.0, 131.0, 132.0, 133.0, 134.0, 135.0, 136.0, 137.0, 138.0, 139.0, 140.0, 141.0, 142.0, 143.0, 144.0, 145.0, 146.0, 147.0, 148.0, 149.0, 150.0, 151.0, 152.0, 153.0, 154.0, 155.0, 156.0, 157.0, 158.0, 159.0, 160.0, 161.0, 162.0, 163.0, 164.0, 165.0, 166.0, 167.0, 168.0, 169.0, 170.0, 171.0, 172.0, 173.0, 174.0, 175.0, 176.0, 177.0, 178.0, 179.0, 180.0, 181.0, 182.0, 183.0, 184.0, 185.0, 186.0, 187.0, 188.0, 189.0, 190.0, 191.0, 192.0, 193.0, 194.0, 195.0, 196.0, 197.0, 198.0, 199.0, 200.0, 201.0, 202.0, 203.0, 204.0, 205.0, 206.0, 207.0, 208.0, 209.0, 210.0, 211.0, 212.0, 213.0, 214.0, 215.0, 216.0, 217.0, 218.0, 219.0, 220.0]
+            w.writerow(head)
+            for peak in nin:
+
+                area = peak_sum_area(im, peak)
+                #print('area=', area)
+                peak.set_area(area)
+                ms = peak.get_mass_spectrum()
+                #print("Peaks rt: ", peak.get_rt())
+                #print("Peaks ms_list: ", ms.mass_list)
+                #print("Peaks ms_spec: ", list(ms.mass_spec))
+                p_rt = peak.get_rt()
+                its = []
+                items = list(ms.mass_spec)
+                for i in items:
+                    x = float(i)
+                    its.append(x)
+                ms_d = ([p_rt] + its)
+                #print(ms_d)
+
+
+                w.writerow(ms_d)
+        f.close()
+
+
+        peakz.append(nin)
+        #print("...", counter)
+        counter += 1
+        ms_data_files.append(completeName)
+
+
+    return [peakz, ms_data_files]
+
+
+def MS_process(file_list):
+    ratio_set = []
+    peaks = []
+
+    for n in file_list:
+        with open(n, 'r') as f:
+            next(f)
+            for line in f:
+                sline = line.split(',')
+                p = sline[0]
+                peaks.append(p)
+                ratios = []
+                maxi = max(map(float, sline))
+                loc = sline.index(str(maxi))
+
+
+                c = 34.0
+                for i in sline:
+                    r = float(i) / float(maxi)
+                    rx = int(r * 999)
+                    ratios.append([c, rx])
+                    c += 1
+
+
+                ratio_set.append(ratios)
+
+            for ratios, p in zip(ratio_set, peaks):
+                # print(ratios)
+                rs = sorted(ratios, key=lambda t: t[1], reverse=True)[:10]
+                # print(rs)
+                print('Name: peak #', p)
+                print('Num Peaks:', 10)
+                for i in rs:
+                    print(str(i).replace('[', '').replace(']', '').replace(',', ''))
+                print('\n')
+
+
+def Experiment_store(names, peakz, name_tag, sdir):
+
+    for n, p in itertools.izip(names, peakz):
+        expr = Experiment(n, p)
+        expr.sele_rt_range(["1m", "50m"])
+        store_expr(sdir+n+name_tag+"expr", expr)
+        print(n, "checked")
+
+
+def main():
+
+    print("Enter values for: cdf file directory, points, scans, percent, num. of ions, storage directory" )
+
+    dirc = raw_input("CDF file directory:")
+    points = int(raw_input("Points:"))
+    scans = int(raw_input("Scans:"))
+    percent = int(raw_input("Percent:"))
+    nin = int(raw_input("num. of ions:"))
+    sdir = raw_input("Storage directory:")
+    matrixes = []
+    noise = []
+    name_tag = str('p'+str(points)+'s'+str(scans)+'%'+str(percent)+'n'+str(nin))
+
+
+    print ("CDF file directory:", dirc)
+    print ("Points:", points)
+    print ("Scans:", scans)
+    print ("Percent:", percent)
+    print ("num. of ions:", nin)
+    print("Name_tag:", name_tag)
+    print("Storage directory:", sdir)
+
+
+    list_of_cdffiles, names = glob(glob_pattern='*.cdf', directoryname=dirc)
+    for cdffile, name in itertools.izip(list_of_cdffiles, names):
+        # print name
+        names.append(name)
+        m_c = matrix_from_cdf(cdffile, name)
+        matrixes.append(m_c[0])
+        noise.append(m_c[1])
+
+    pp_im = Preprocess_IntensityMatrixes(matrixes)
+    # for i, n in itertools.izip(pp_im, noise):
+    #     print(i, n)
+    peak_m = Peak_detector(pp_im, noise, names, points, scans, percent, nin, name_tag, sdir)
+    Experiment_store(names, peak_m[0], name_tag, sdir)
+
+
+    #print(dirc, points)
+
+
+if __name__ == "__main__":
+    main()
