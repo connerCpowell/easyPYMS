886f4acf34e93f7fea3a1bdffe1f4ff38798d6dc
connerCpowell <copowengineering@gmail.com>
2020-07-02 15:42:43 -0400

    Adding Lon's code review suggestions

--- ppB.py
+++ ppB.py
@@ -1,4 +1,4 @@
-import sys
+#import sys
 from pyms.GCMS.IO.ANDI.Function import ANDI_reader
 from pyms.GCMS.Function import build_intensity_matrix
 from pyms.Noise.SavitzkyGolay import savitzky_golay
@@ -15,7 +15,7 @@ from pyms.Noise.Analysis import window_analyzer
 import csv
 import argparse
 
-import re
+#import re
 
 from datetime import datetime
 
@@ -103,7 +103,7 @@ def Peak_detector(pp_im, noise, name, points, scans, percent, ni, name_tag, sdir
 
     for im, n, na in itertools.izip(list(pp_im), noise, name):
 
-        ms_data = []
+        #ms_data = []
 
         # print(na)
         poss_peaks = BillerBiemann(im, points=points, scans=scans)  # increase scan #
@@ -113,20 +113,8 @@ def Peak_detector(pp_im, noise, name, points, scans, percent, ni, name_tag, sdir
         completeName = os.path.join(savePath, na + name_tag + "ms_data.csv")
         with open(completeName, 'w') as f:
             w = csv.writer(f)
-            # head = [35.0, 36.0, 37.0, 38.0, 39.0, 40.0, 41.0, 42.0, 43.0, 44.0, 45.0, 46.0, 47.0, 48.0, 49.0, 50.0, 51.0, 52.0, 53.0, 54.0, 55.0, 56.0, 57.0, 58.0, 59.0, 60.0, 61.0, 62.0, 63.0, 64.0, 65.0, 66.0, 67.0, 68.0, 69.0, 70.0, 71.0, 72.0, 73.0, 74.0, 75.0, 76.0, 77.0, 78.0, 79.0, 80.0, 81.0, 82.0, 83.0, 84.0, 85.0, 86.0, 87.0, 88.0, 89.0, 90.0, 91.0, 92.0, 93.0, 94.0, 95.0, 96.0, 97.0, 98.0, 99.0, 100.0, 101.0, 102.0, 103.0, 104.0, 105.0, 106.0, 107.0, 108.0, 109.0, 110.0, 111.0, 112.0, 113.0, 114.0, 115.0, 116.0, 117.0, 118.0, 119.0, 120.0, 121.0, 122.0, 123.0, 124.0, 125.0, 126.0, 127.0, 128.0, 129.0, 130.0, 131.0, 132.0, 133.0, 134.0, 135.0, 136.0, 137.0, 138.0, 139.0, 140.0, 141.0, 142.0, 143.0, 144.0, 145.0, 146.0, 147.0, 148.0, 149.0, 150.0, 151.0, 152.0, 153.0, 154.0, 155.0, 156.0, 157.0, 158.0, 159.0, 160.0, 161.0, 162.0, 163.0, 164.0, 165.0, 166.0, 167.0, 168.0, 169.0, 170.0, 171.0, 172.0, 173.0, 174.0, 175.0, 176.0, 177.0, 178.0, 179.0, 180.0, 181.0, 182.0, 183.0, 184.0, 185.0, 186.0, 187.0, 188.0, 189.0, 190.0, 191.0, 192.0, 193.0, 194.0, 195.0, 196.0, 197.0, 198.0, 199.0, 200.0, 201.0, 202.0, 203.0, 204.0, 205.0, 206.0, 207.0, 208.0, 209.0, 210.0, 211.0, 212.0, 213.0, 214.0, 215.0, 216.0, 217.0, 218.0, 219.0, 220.0]
-            head = ['Area', 'RTs', 35.0, 36.0, 37.0, 38.0, 39.0, 40.0, 41.0, 42.0, 43.0, 44.0, 45.0, 46.0, 47.0, 48.0,
-                    49.0, 50.0, 51.0, 52.0, 53.0, 54.0, 55.0, 56.0, 57.0, 58.0, 59.0, 60.0, 61.0, 62.0, 63.0, 64.0,
-                    65.0, 66.0, 67.0, 68.0, 69.0, 70.0, 71.0, 72.0, 73.0, 74.0, 75.0, 76.0, 77.0, 78.0, 79.0, 80.0,
-                    81.0, 82.0, 83.0, 84.0, 85.0, 86.0, 87.0, 88.0, 89.0, 90.0, 91.0, 92.0, 93.0, 94.0, 95.0, 96.0,
-                    97.0, 98.0, 99.0, 100.0, 101.0, 102.0, 103.0, 104.0, 105.0, 106.0, 107.0, 108.0, 109.0, 110.0,
-                    111.0, 112.0, 113.0, 114.0, 115.0, 116.0, 117.0, 118.0, 119.0, 120.0, 121.0, 122.0, 123.0, 124.0,
-                    125.0, 126.0, 127.0, 128.0, 129.0, 130.0, 131.0, 132.0, 133.0, 134.0, 135.0, 136.0, 137.0, 138.0,
-                    139.0, 140.0, 141.0, 142.0, 143.0, 144.0, 145.0, 146.0, 147.0, 148.0, 149.0, 150.0, 151.0, 152.0,
-                    153.0, 154.0, 155.0, 156.0, 157.0, 158.0, 159.0, 160.0, 161.0, 162.0, 163.0, 164.0, 165.0, 166.0,
-                    167.0, 168.0, 169.0, 170.0, 171.0, 172.0, 173.0, 174.0, 175.0, 176.0, 177.0, 178.0, 179.0, 180.0,
-                    181.0, 182.0, 183.0, 184.0, 185.0, 186.0, 187.0, 188.0, 189.0, 190.0, 191.0, 192.0, 193.0, 194.0,
-                    195.0, 196.0, 197.0, 198.0, 199.0, 200.0, 201.0, 202.0, 203.0, 204.0, 205.0, 206.0, 207.0, 208.0,
-                    209.0, 210.0, 211.0, 212.0, 213.0, 214.0, 215.0, 216.0, 217.0, 218.0, 219.0, 220.0]
+            head = ['Area', 'RTs'] + [float(i) for i in range(35,221)]
+
 
             w.writerow(head)
             for peak in nin:
@@ -140,10 +128,10 @@ def Peak_detector(pp_im, noise, name, points, scans, percent, ni, name_tag, sdir
                 # print("Peaks ms_spec: ", list(ms.mass_spec))
                 p_rt = peak.get_rt()
                 its = []
-                items = list(ms.mass_spec)
-                for i in items:
-                    x = float(i)
-                    its.append(x)
+                ms_items = list(ms.mass_spec)
+                for spec in ms_items:
+                    f_spec = float(spec)
+                    its.append(f_spec)
 
                 ms_d = ([area] + [p_rt] + its)
                 # ms_d = its
@@ -161,6 +149,13 @@ def Peak_detector(pp_im, noise, name, points, scans, percent, ni, name_tag, sdir
     return [peakz, ms_data_files]
 
 
+def replacementWriter(ll, pp, chars):
+    ll = str(ll)
+    for c in chars:
+        ll = ll.replace(c, '')
+    pp.write(ll + "\n")
+
+
 def MS_process(file_list):
     '''
     @param file_list:
@@ -174,7 +169,7 @@ def MS_process(file_list):
         peaks = []
         areas = []
         print('n=', n)
-        print("-------------11------------------------------------")
+        #print("-------------11------------------------------------")
         with open(n, 'r') as f:
             next(f)
             for line in f:
@@ -208,35 +203,50 @@ def MS_process(file_list):
             # print('peaks=', peaks)
             # print('areas=', areas)
 
-            print("----------22--------------------------------------")
+            #print("----------22--------------------------------------")
             name1 = n.rsplit('ms_data.csv')
             name2 = name1[0] + '.txt'
             print('n2=', name2)
+
+            with open(name2, "w+") as pp:
+                for ratios, p, a, in zip(ratio_set, peaks, areas):
+                    replacementWriter(ll=('Name:', p, 'Area-', a), pp=pp, chars="',()")
+                    replacementWriter(ll=('Num Peaks:', 10), pp=pp, chars="',()")
+
+                    for i in sorted(ratios, key=lambda t: t[1], reverse=True)[:10]:
+                        replacementWriter(ll=i, pp=pp, chars="[],")
+                    pp.write("\n")
+
+
+
+
             # ms_name = os.path.join()
             # print('ms', name1)
 
-            pp = open(name2, "w+")
-            for ratios, p, a in zip(ratio_set, peaks, areas):
-                # print('ratios=', ratios)
-                rs = sorted(ratios, key=lambda t: t[1], reverse=True)[:10]
+            # pp = open(name2, "w+")
+            # for ratios, p, a in zip(ratio_set, peaks, areas):
+            #     # print('ratios=', ratios)
+            #     rs = sorted(ratios, key=lambda t: t[1], reverse=True)[:10]
+            #
+            #     # print('rs=', rs)
+            #     ll = 'Name:', p, 'Area-', a
+            #     ll = str(ll).replace("'", "").replace(",", "").replace("(", "").replace(")", "")
+            #     # print('ll=', ll)
+            #     pp.write(ll + "\n")
+            #
+            #     nn = 'Num Peaks:', 10
+            #     nn = str(nn).replace("'", "").replace(",", "").replace("(", "").replace(")", "")
+            #     # print('nn=', nn)
+            #     pp.write(nn + "\n")
+            #     for i in rs:
+            #         ss = str(i).replace('[', '').replace(']', '').replace(',', '')
+            #         # print('ss=', ss)
+            #         pp.write(ss + "\n")
+            #     # print('\n')
+            #     pp.write("\n")
+            # pp.close()
 
-                # print('rs=', rs)
-                ll = 'Name:', p, 'Area-', a
-                ll = str(ll).replace("'", "").replace(",", "").replace("(", "").replace(")", "")
-                # print('ll=', ll)
-                pp.write(ll + "\n")
 
-                nn = 'Num Peaks:', 10
-                nn = str(nn).replace("'", "").replace(",", "").replace("(", "").replace(")", "")
-                # print('nn=', nn)
-                pp.write(nn + "\n")
-                for i in rs:
-                    ss = str(i).replace('[', '').replace(']', '').replace(',', '')
-                    # print('ss=', ss)
-                    pp.write(ss + "\n")
-                # print('\n')
-                pp.write("\n")
-            pp.close()
 
             # nameFil = name1[0] + '.FIL'
             # ff = open(nameFil, "w+")
@@ -334,29 +344,19 @@ def main():
                         help="EXPR Directory: Location to save the .expr files for alignment scripts \n")
     args = parser.parse_args()
 
-    dirc = args.dirc
-    sp = args.name
-    points = args.points
-    scans = args.scans
-    percent = args.threshold
-    nin = args.ion
-    sdir = args.sdir
-    sdir2 = args.sdir2
     print(args)
 
+    name_tag = str('p' + str(args.points) + 's' + str(args.scans) + '%' + str(args.threshold) + 'n' + str(args.ion))
 
-
-    name_tag = str('p' + str(points) + 's' + str(scans) + '%' + str(percent) + 'n' + str(nin))
-
-    print("CDF file directory:", dirc)
-    print("split:", sp)
-    print("Points:", points)
-    print("Scans:", scans)
-    print("Percent:", percent)
-    print("num. of ions:", nin)
+    print("CDF file directory:", args.dirc)
+    print("split:", args.name)
+    print("Points:", args.points)
+    print("Scans:", args.scans)
+    print("Percent:", args.threshold)
+    print("num. of ions:", args.ion)
     print("Name_tag:", name_tag)
-    print("Storage directory (csv):", sdir)
-    print("Storage dir (expr):", sdir2)
+    print("Storage directory (csv):", args.sdir)
+    print("Storage dir (expr):", args.sdir2)
 
     matrixes = []
     noise = []
@@ -364,7 +364,7 @@ def main():
     startTime = datetime.now()
 
     # Glob command used to locate .cdf files and create a list of the files
-    list_of_cdffiles, names = glob(glob_pattern='*.cdf', directoryname=dirc, splitPattern=sp)
+    list_of_cdffiles, names = glob(glob_pattern='*.cdf', directoryname=args.dirc, splitPattern=args.name)
 
     for cdffile, name in itertools.izip(list_of_cdffiles, names):
         print('name=', name)
@@ -378,15 +378,18 @@ def main():
     pp_im = Preprocess_IntensityMatrixes(matrixes)
     # for i, n in itertools.izip(pp_im, noise):
     #     print(i, n)
-    peak_m = Peak_detector(pp_im, noise, names, points, scans, percent, nin, name_tag, sdir)
-    Experiment_store(names, peak_m[0], name_tag, sdir2)
+    peak_m = Peak_detector(pp_im, noise, names, args.points, args.scans, args.threshold, args.ion, name_tag, args.sdir)
+    Experiment_store(names, peak_m[0], name_tag, args.sdir2)
 
     print('p1=', peak_m[1])
     MS_process(peak_m[1])
 
-    print("runtime=", (datetime.now() - startTime))
+    timeR = datetime.now() - startTime
+    #tiM = timeR/60
+    print('Seconds:', str(timeR))
+    #print('Minutes', str(tiM))
 
-    # print(dirc, points)
+    # print(args.dirc, points)
 
 
 if __name__ == "__main__":
