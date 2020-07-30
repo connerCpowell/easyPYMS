421320384a3e1222611abe1e39f4a0454df6e6ec
connerCpowell <copowengineering@gmail.com>
2020-07-30 10:21:14 -0700

    renaming runTXT to ppGCMS.sh

---
+++ ppGCMS.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'python preppeaks_GCMS.py \
+-f /workdir2/shared_folder/cpowell/fielTmp/all/xab \
+-n /2016_blueberry_gwas/ \ 
+-p 140 \
+-s 25 \
+-t 3 \
+-i 3 \
+-c /workdir2/shared_folder/cpowell/expr \
+-e /workdir2/shared_folder/cpowell/expr'
